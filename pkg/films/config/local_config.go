a2df27c0a8057b1673b37deaf51959592a4dbda5
vojtech hrabal <vojtech.hrabal@cz.ibm.com>
2022-02-16 01:11:59 +0100

    add env config

---
+++ pkg/films/config/local_config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	App_env           string `env:"app_env,required" envDefault:"DEVELOPMENT"`
+	Token             string `env:"token"`
+	Debug_addr        string `env:"debug_addr,required,notEmpty" envDefault:":8080"`
+	Http_addr         string `env:"http_addr,required,notEmpty" envDefault:":8081"`
+	Channel           string `env:"gokit-users"`
+	Postgres_database string `env:"postgres_database,required,notEmpty" envDefault:"dvdrental"`
+	Postgres_dbuser   string `env:"postgres_dbuser,required,notEmpty" envDefault:"postgres"`
+	Postgres_host     string `env:"postgres_host,required,notEmpty" envDefault:"localhost"`
+	Postgres_port     string `env:"postgres_port,required,notEmpty" envDefault:"5432"`
+	Postgres_password string `env:"postgres_password,required,notEmpty,unset" envDefault:"secret"`
+	Postgres_sslmode  string `env:"postgres_sslmode,required,notEmpty" envDefault:"disable"`
+	Postgres_timezone string `env:"postgres_timezone,required,notEmpty" envDefault:"Europe/Prague"`
+}
+
+func InitConfig() (Config, error) {
+	cfg := Config{}
+	if err := env.Parse(&cfg); err != nil {
+		return Config{}, err
+	}
+	return cfg, nil
+}
