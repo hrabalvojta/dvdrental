e1d74139cf5ac10faef3dbdc65f3c7cd60da56ce
vojtech hrabal <vojtech.hrabal@cz.ibm.com>
2022-04-14 19:22:45 +0200

    move to psql dsn, add psql opt

--- internal/config/local_config.go
+++ internal/config/local_config.go
@@ -5,18 +5,16 @@ import (
 )
 
 type Config struct {
-	App_env           string `env:"app_env,required" envDefault:"DEVELOPMENT"`
-	Token             string `env:"token"`
-	Debug_addr        string `env:"debug_addr,required,notEmpty" envDefault:":8080"`
-	Http_addr         string `env:"http_addr,required,notEmpty" envDefault:":8081"`
-	Channel           string `env:"gokit-films" envDefault:"gokit-films"`
-	Postgres_database string `env:"postgres_database,required,notEmpty" envDefault:"dvdrental"`
-	Postgres_dbuser   string `env:"postgres_dbuser,required,notEmpty" envDefault:"postgres"`
-	Postgres_host     string `env:"postgres_host,required,notEmpty" envDefault:"localhost"`
-	Postgres_port     string `env:"postgres_port,required,notEmpty" envDefault:"5432"`
-	Postgres_password string `env:"postgres_password,required,notEmpty,unset" envDefault:"secret"`
-	Postgres_sslmode  string `env:"postgres_sslmode,required,notEmpty" envDefault:"disable"`
-	Postgres_timezone string `env:"postgres_timezone,required,notEmpty" envDefault:"Europe/Prague"`
+	// Application
+	App_env    string `env:"app_env,required" envDefault:"DEVELOPMENT"`
+	Token      string `env:"token"`
+	Debug_addr string `env:"debug_addr,required,notEmpty" envDefault:":8080"`
+	Http_addr  string `env:"http_addr,required,notEmpty" envDefault:":8081"`
+	Channel    string `env:"gokit-films" envDefault:"gokit-films"`
+
+	// DATABASE PSQL
+	Postgres_dsn     string `env:"postgres_database,required,notEmpty" envDefault:"postgres://postgres:secret@localhost:5432/dvdrental?sslmode=disable"`
+	Postgres_timeout int    `env:"postgres_timeout" envDefault:"1"`
 }
 
 func InitConfig() (Config, error) {
